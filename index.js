b96203217318fcd926d2e32ce00ffa42059a9f50
Solemn-Demons <128663691+Solemn-Demons@users.noreply.github.com>
2023-10-11 17:52:50 -0400

    Attempt 1

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
 const inquirer = require("inquirer")
+const { triangle, square, circle, star } = require ('./svg/options.js')
+
+class CLI {
+    run() {
+        return inquirer 
+        .prompt ([
+        {
+            name: "text", 
+            type: "input",
+            message: "Add at least two characters"
+            validate: (input) =>
+            input.length <= 2 ||
+        },
+        {
+            name: "shape",
+            type: "list",
+            message: "select shape",
+            choices: ["triangle", "square", "circle", "star"],
+        },
+        {
+            name: "textColor",
+            type: "input",
+            message: "Add color"
+        },
+        {
+            name: "shapeColor",
+            type: "input"
+            message: "Add color"
+        },
+    ])
+    .then(({ text, shape, textColor, shapeColor}) => {
+        let shape;
+        switch (shape) {
+            case "triangle":
+                shape = new triangle(text, textColor, shapeColor);
+                break;
+            case "square":
+                shape = new square(text, textColor, shapeColor);
+                break;
+            case "circle":
+                shape = new circle(text, textColor, shapeColor);
+                break;
+            case "star":
+                shape = new star(text, textColor, shapeColor);
+                break;
+        }
+    })
+    const svg = new SVG();
+    svg.setText(text, textColor);
+    svg.setShape(shape, shapeColor);
+    return writeFile("logo.svg", svg.render());
+}
+
+module.exports = CLI;
---
+++ svg/options.js
@@ -0,0 +1,11 @@
+
+class Shape {
+    constructor(text, textColor, shape, shapeColor) {
+        (this.text = text),
+        (this.textColor = textColor),
+        (this.shape = shape),
+        (this.shapeColor = shapeColor);
+    }
+}
+
+module.exports = { triangle, circle, square, star};
